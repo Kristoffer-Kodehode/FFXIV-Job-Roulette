7168fb24223f1ecd3cd1a546e78856e4c110e025
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2024-01-16 13:15:28 +0100

    ++reroll & all roles

--- FFXIV_Job_Roulette.cpp
+++ FFXIV_Job_Roulette.cpp
@@ -3,8 +3,14 @@
 #include "job_arrays.hpp"
 
 int roll(std::vector<std::string> type);
+
 void roll_all();
 void roll_tanks();
+void roll_healers();
+void roll_dps();
+void roll_melee();
+void roll_ranged();
+void roll_magic();
 
 int main() {
     //setup input for role choice
@@ -25,6 +31,16 @@ int main() {
         roll_all();
     } else if (role == "tank") {
         roll_tanks();
+    } else if (role == "healer") {
+        roll_healers();
+    } else if (role == "dps") {
+        roll_dps();
+    } else if (role == "melee") {
+        roll_melee();
+    } else if (role == "ranged") {
+        roll_ranged();
+    } else if (role == "magic") {
+        roll_magic();
     } else {
         std::cout << "Invalid input.\n";
     }
@@ -39,14 +55,126 @@ int main() {
 }
 
 void roll_all() {
+    char reroll;
+
     int random_index = roll(all_jobs);
     std::cout << all_jobs[random_index] << "\n";
+
+    std::cout << "Reroll? (y/n) ";
+    std::cin >> reroll;
+
+    if (reroll == 'y' || reroll == 'Y') {
+        roll_all();
+    } else {
+        return;
+    }
 }
 
 void roll_tanks() {
+    char reroll;
+
     int random_index = roll(tanks);
     std::cout << tanks[random_index] << "\n";
+
+    std::cout << "Reroll? (y/n) ";
+    std::cin >> reroll;
+
+    if (reroll == 'y' || reroll == 'Y') {
+        roll_tanks();
+    } else {
+        return;
+    }
 }
+
+void roll_healers() {
+    char reroll;
+
+    int random_index = roll(tanks);
+    std::cout << healers[random_index] << "\n";
+    
+    std::cout << "Reroll? (y/n) ";
+    std::cin >> reroll;
+
+    if (reroll == 'y' || reroll =='Y') {
+        roll_healers();
+    } else {
+        return;
+    }
+}
+
+void roll_dps() {
+    char reroll;
+    
+    int random_index = roll(all_dps);
+    std::cout << all_dps[random_index] << "\n";
+
+    std::cout << "Reroll? (y/n) ";
+    std::cin >> reroll;
+
+    if (reroll == 'y' || reroll == 'Y') {
+        roll_dps();
+    } else {
+        return;
+    }
+}
+
+void roll_melee() {
+    char reroll;
+
+    int random_index = roll(melee);
+    std::cout << melee[random_index] << "\n";
+
+    std::cout << "Reroll? (y/n) ";
+    std::cin >> reroll;
+
+    if (reroll == 'y' || reroll == 'Y') {
+        roll_melee();
+    } else {
+        return;
+    }
+}
+
+void roll_ranged() {
+    char reroll;
+
+    int random_index = roll(ranged);
+    std::cout << ranged[random_index] << "\n";
+
+    std::cout << "Reroll? (y/n) ";
+    std::cin >> reroll;
+
+    if (reroll == 'y' || reroll == 'Y') {
+        roll(ranged);
+    } else {
+        return;
+    }
+}
+
+void roll_magic() {
+    char reroll;
+
+    int random_index = roll(magic);
+    std::cout << magic[random_index] << "\n";
+
+    std::cout << "Reroll (y/n) ";
+    std::cin >> reroll;
+
+    if (reroll == 'y' || reroll == 'Y') {
+        roll_magic();
+    } else {
+        return;
+    }
+}
+
+/*void reroll(void rollfunc) { attempt at a reroll function
+    std::cout << "Reroll? (y/n) ";
+    std::cin >> reroll;
+    if (reroll == 'y') {
+        rollfunc;
+    } else {
+        return;
+    }
+}/*/
 
 int roll(std::vector<std::string> type) {
     srand(time(NULL));
